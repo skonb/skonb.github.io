da25bbf84d5c2040f8910fad72a6ae1efa59f24e
skonb <w0rkingc4t0708@gmail.com>
2021-08-14 22:51:44 +0900

    Add briks and make collision detection

--- src/views/GameCorner.vue
+++ src/views/GameCorner.vue
@@ -13,7 +13,8 @@
 <script lang='ts'>
 import { defineComponent, onMounted, ref } from 'vue';
 import {useRouter} from 'vue-router';
-
+//ブロックを書く
+            
 export default defineComponent({
     e: '#game_corner',
     //data・methods・computed・ライフサイクルメソッドなどの区別がなくなり、全てがsetupメソッドの中に記述される
@@ -40,6 +41,21 @@ export default defineComponent({
         let paddleX:number;
         let cheatMode=false;
 
+        let brickRowCount = 3;
+        let brickColumnCount = 5;
+        let brickWidth = 75;
+        let brickHeight = 20;
+        let brickPadding = 10;
+        let brickOffsetTop = 30;
+        let brickOffsetLeft = 30;
+        let bricks:Array<Array<any>>=[];
+        for (let c=0;c<brickColumnCount;c++){
+            bricks[c]=[];
+            for (let r=0;r<brickRowCount;r++){
+                bricks[c][r] = {x:0,y:0,state:1};
+            }
+        }
+
         onMounted(async () => {
             x = my_canvas.value!.width/2;
             y = my_canvas.value!.height-30;
@@ -54,12 +70,59 @@ export default defineComponent({
             window.addEventListener('keydown', keyDownHandler, false);
             window.addEventListener('keyup', keyUpHandler, false);
         });
+        //function about brics
+        const drawBricks = () => {
+            const ctx = my_canvas.value?.getContext('2d')!;
+            for(var c=0; c<brickColumnCount; c++) {
+                for(var r=0; r<brickRowCount; r++) {
+                    if (bricks[c][r].state==1){
+                        let brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+                        let brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+                        bricks[c][r].x = brickX;
+                        bricks[c][r].y = brickY;
+                        ctx.beginPath();
+                        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                        ctx.fillStyle = '#0095DD';
+                        ctx.fill();
+                        ctx.closePath();
+
+                    }
+                }
+            }
+        };
+        /**
+         * 衝突を検知する
+         */
+        const collisionDetection = () => {
+            for(var c=0; c<brickColumnCount; c++) {
+                for(var r=0; r<brickRowCount; r++) {
+                    let b = bricks[c][r];
+                    //衝突条件は
+                    // 0. b.state==1(blockがまだ消えていない)
+                    // 1. brickX<ballX-r
+                    // 2. brickY<ballY-r
+                    // 3. brickX + brickWidth >ballX+r
+                    // 4. brickY + brickHeight >ballY+r
+                    let cond0 = b.state==1;
+                    let cond1 = b.x<x-r;
+                    let cond2 = b.y<y-r;
+                    let cond3 = b.x+brickWidth>x+r;
+                    let cond4 = b.y+brickHeight>y+r;
+                    if (cond0 &&cond1 && cond2 && cond3 && cond4){
+                        dy *=-1;
+                        b.state=0;
+                    }
+                }
+            }
+            
+        };
         const toggleCheat=()=>{
             cheatMode = !cheatMode;
         };
         const keyDownHandler = (e:KeyboardEvent) => {
             if (e.key=='Right'||e.key=='ArrowRight'){
-                rightPressed = true;
+                rightPressed = true//ブロックを書く
+                ;
             } else if (e.key == 'Left'||e.key=='ArrowLeft'){
                 leftPressed= true;
             }
@@ -109,11 +172,16 @@ export default defineComponent({
             ctx.strokeRect(0,0,my_canvas.value!.width, my_canvas.value!.height);
             //円を描く
             ctx.arc(x, y, r, 0, Math.PI*2, false);//円の中心x,円の中心y,円の半径, 開始角度と終了角度，描く方向(falseなら時計回り)
+            
+            //ブロックを書く
+
 
             ctx.fillStyle = 'green';
             ctx.fill();
             ctx.closePath();
             drawPaddle();
+            collisionDetection();
+            drawBricks();
             x += dx;
             y += dy;
             if (dx<0 && x-r<0||dx>0&&x+r>my_canvas.value!.width){
